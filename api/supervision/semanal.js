a1387e4458d66bc15a6442d9ea89eec2ae4235c4
memelandia <francoponce1994@hotmail.com>
2025-12-29 13:23:43 +0100

    Fix: Cast JSON string to JSONB in SQL query

--- api/supervision/semanal.js
+++ api/supervision/semanal.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
       for (const row of data) {
         await sql`
           INSERT INTO supervision_semanal (id, data)
-          VALUES (${row.id}, ${JSON.stringify(row)})
+          VALUES (${row.id}, ${JSON.stringify(row)}::jsonb)
         `;
       }
 
